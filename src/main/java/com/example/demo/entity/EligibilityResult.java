a8152176a71f116b661773ea10ff212304210f61
bala-amypo <bala@amypo.in>
2025-12-17 09:01:38 +0000

    User commit

--- src/main/java/com/example/demo/entity/EligibilityResult.java
+++ src/main/java/com/example/demo/entity/EligibilityResult.java
@@ -2,5 +2,10 @@ package com.example.demo.entity;
 public class EligibilityResult{
     private Long id;
     private String loanRequest;
-    private 
+    private Boolean isEligible;
+    private Double maxEligibleAmount;
+    private Double EstimatedEmi;
+    private String riskLevel;
+    private String rejectionReason;
+    private Timestamp calculatedAt;
 }
--- src/main/java/com/example/demo/entity/RiskAssessmentLog.java
+++ src/main/java/com/example/demo/entity/RiskAssessmentLog.java
@@ -1,4 +1,9 @@
 package com.example.demo.entity;
 public class RiskAssessmentLog{
-    
+    private Long id;
+    private Long loanRequestId;
+    private Double dtiRatio;
+    private String creditCheckStatus;
+    private Time
+
 }
