34cdb2c34579290fa4844c68ce2bfb8d4b6837fe
bala-amypo <bala@amypo.in>
2025-12-17 09:59:39 +0000

    User commit

--- src/main/java/com/example/demo/entity/FinancialProfile.java
+++ src/main/java/com/example/demo/entity/FinancialProfile.java
@@ -1,92 +1,92 @@
-package com.example.demo.entity;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-@Entity
-public class FinancialProfile{
-    @Id
-    private Long id;
-    private String user;
-    private Double monthlyIncome;
-    private Double monthlyExpenses;
-    private Double existingLoanEmi;
-    private Integer creditScore;
-    private Double savingsBalance;
-    private Timestamp lastUpdatedAt;
-    public Long getId() {
-    return id;
-}
+// package com.example.demo.entity;
+// import jakarta.persistence.Entity;
+// import jakarta.persistence.Id;
+// @Entity
+// public class FinancialProfile{
+//     @Id
+//     private Long id;
+//     private String user;
+//     private Double monthlyIncome;
+//     private Double monthlyExpenses;
+//     private Double existingLoanEmi;
+//     private Integer creditScore;
+//     private Double savingsBalance;
+//     private Timestamp lastUpdatedAt;
+//     public Long getId() {
+//     return id;
+// }
 
-public void setId(Long id) {
-    this.id = id;
-}
+// public void setId(Long id) {
+//     this.id = id;
+// }
 
-public String getUser() {
-    return user;
-}
+// public String getUser() {
+//     return user;
+// }
 
-public void setUser(String user) {
-    this.user = user;
-}
+// public void setUser(String user) {
+//     this.user = user;
+// }
 
-public Double getMonthlyIncome() {
-    return monthlyIncome;
-}
+// public Double getMonthlyIncome() {
+//     return monthlyIncome;
+// }
 
-public void setMonthlyIncome(Double monthlyIncome) {
-    this.monthlyIncome = monthlyIncome;
-}
+// public void setMonthlyIncome(Double monthlyIncome) {
+//     this.monthlyIncome = monthlyIncome;
+// }
 
-public Double getMonthlyExpenses() {
-    return monthlyExpenses;
-}
+// public Double getMonthlyExpenses() {
+//     return monthlyExpenses;
+// }
 
-public void setMonthlyExpenses(Double monthlyExpenses) {
-    this.monthlyExpenses = monthlyExpenses;
-}
+// public void setMonthlyExpenses(Double monthlyExpenses) {
+//     this.monthlyExpenses = monthlyExpenses;
+// }
 
-public Double getExistingLoanEmi() {
-    return existingLoanEmi;
-}
+// public Double getExistingLoanEmi() {
+//     return existingLoanEmi;
+// }
 
-public void setExistingLoanEmi(Double existingLoanEmi) {
-    this.existingLoanEmi = existingLoanEmi;
-}
+// public void setExistingLoanEmi(Double existingLoanEmi) {
+//     this.existingLoanEmi = existingLoanEmi;
+// }
 
-public Integer getCreditScore() {
-    return creditScore;
-}
+// public Integer getCreditScore() {
+//     return creditScore;
+// }
 
-public void setCreditScore(Integer creditScore) {
-    this.creditScore = creditScore;
-}
+// public void setCreditScore(Integer creditScore) {
+//     this.creditScore = creditScore;
+// }
 
-public Double getSavingsBalance() {
-    return savingsBalance;
-}
+// public Double getSavingsBalance() {
+//     return savingsBalance;
+// }
 
-public void setSavingsBalance(Double savingsBalance) {
-    this.savingsBalance = savingsBalance;
-}
+// public void setSavingsBalance(Double savingsBalance) {
+//     this.savingsBalance = savingsBalance;
+// }
 
-public Timestamp getLastUpdatedAt() {
-    return lastUpdatedAt;
-}
+// public Timestamp getLastUpdatedAt() {
+//     return lastUpdatedAt;
+// }
 
-public void setLastUpdatedAt(Timestamp lastUpdatedAt) {
-    this.lastUpdatedAt = lastUpdatedAt;
-}
-public FinancialProfile(Long id,String user,Double monthlyIncome,Double monthlyExpenses,Double existingLoanEmi,Integer creditScore,Double savingsBalance,Timestamp lastUpdatedAt){
-    this.id=id;
-    this.user=user;
-    this.monthlyIncome=monthlyIncome;
-    this.monthlyExpenses=monthlyExpenses;
-    this.existingLoanEmi=existingLoanEmi;
-    this.creditScore=creditScore;
-    this.savingsBalance=savingsBalance;
-    this.lastUpdatedAt=lastUpdatedAt;
-}
-public FinancialProfile(){
+// public void setLastUpdatedAt(Timestamp lastUpdatedAt) {
+//     this.lastUpdatedAt = lastUpdatedAt;
+// }
+// public FinancialProfile(Long id,String user,Double monthlyIncome,Double monthlyExpenses,Double existingLoanEmi,Integer creditScore,Double savingsBalance,Timestamp lastUpdatedAt){
+//     this.id=id;
+//     this.user=user;
+//     this.monthlyIncome=monthlyIncome;
+//     this.monthlyExpenses=monthlyExpenses;
+//     this.existingLoanEmi=existingLoanEmi;
+//     this.creditScore=creditScore;
+//     this.savingsBalance=savingsBalance;
+//     this.lastUpdatedAt=lastUpdatedAt;
+// }
+// public FinancialProfile(){
 
-}
-}
+// }
+// }
 
--- src/main/java/com/example/demo/entity/LoanReqest.java
+++ src/main/java/com/example/demo/entity/LoanReqest.java
@@ -1,84 +1,84 @@
-package com.example.demo.entity;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-@Entity
-public class LoanRequest{
-    @Id
-    private Long id;
-    private String user;
-    private Double reqestedAmount;
-    private Integer tenureMonths;
-    private String purpose;
-    private String status;
-    private Timestamp appliedAt;
-    public Long getId() {
-    return id;
-}
+// package com.example.demo.entity;
+// import jakarta.persistence.Entity;
+// import jakarta.persistence.Id;
+// @Entity
+// public class LoanRequest{
+//     @Id
+//     private Long id;
+//     private String user;
+//     private Double reqestedAmount;
+//     private Integer tenureMonths;
+//     private String purpose;
+//     private String status;
+//     private Timestamp appliedAt;
+//     public Long getId() {
+//     return id;
+// }
 
-public void setId(Long id) {
-    this.id = id;
-}
+// public void setId(Long id) {
+//     this.id = id;
+// }
 
-public String getUser() {
-    return user;
-}
+// public String getUser() {
+//     return user;
+// }
 
-public void setUser(String user) {
-    this.user = user;
-}
+// public void setUser(String user) {
+//     this.user = user;
+// }
 
-public Double getReqestedAmount() {
-    return reqestedAmount;
-}
+// public Double getReqestedAmount() {
+//     return reqestedAmount;
+// }
 
-public void setReqestedAmount(Double reqestedAmount) {
-    this.reqestedAmount = reqestedAmount;
-}
+// public void setReqestedAmount(Double reqestedAmount) {
+//     this.reqestedAmount = reqestedAmount;
+// }
 
-public Integer getTenureMonths() {
-    return tenureMonths;
-}
+// public Integer getTenureMonths() {
+//     return tenureMonths;
+// }
 
-public void setTenureMonths(Integer tenureMonths) {
-    this.tenureMonths = tenureMonths;
-}
+// public void setTenureMonths(Integer tenureMonths) {
+//     this.tenureMonths = tenureMonths;
+// }
 
-public String getPurpose() {
-    return purpose;
-}
+// public String getPurpose() {
+//     return purpose;
+// }
 
-public void setPurpose(String purpose) {
-    this.purpose = purpose;
-}
+// public void setPurpose(String purpose) {
+//     this.purpose = purpose;
+// }
 
-public String getStatus() {
-    return status;
-}
+// public String getStatus() {
+//     return status;
+// }
 
-public void setStatus(String status) {
-    this.status = status;
-}
+// public void setStatus(String status) {
+//     this.status = status;
+// }
 
-public Timestamp getAppliedAt() {
-    return appliedAt;
-}
+// public Timestamp getAppliedAt() {
+//     return appliedAt;
+// }
 
-public void setAppliedAt(Timestamp appliedAt) {
-    this.appliedAt = appliedAt;
-}
-public LoanRequest(Long id,String user,Double reqestedAmount,Integer tenureMonths,String purpose,String status,Timestamp appliedAt){
-    this.id=id;
-    this.user=user;
-    this.reqestedAmount=reqestedAmount;
-    this.tenureMonths=tenureMonths;
-    this.purpose=purpose;
-    this.status=status;
-    this.appliedAt=appliedAt;
+// public void setAppliedAt(Timestamp appliedAt) {
+//     this.appliedAt = appliedAt;
+// }
+// public LoanRequest(Long id,String user,Double reqestedAmount,Integer tenureMonths,String purpose,String status,Timestamp appliedAt){
+//     this.id=id;
+//     this.user=user;
+//     this.reqestedAmount=reqestedAmount;
+//     this.tenureMonths=tenureMonths;
+//     this.purpose=purpose;
+//     this.status=status;
+//     this.appliedAt=appliedAt;
 
 
-}
-public LoanRequest(){
+// }
+// public LoanRequest(){
 
-}
+// }
 
-}
+// }
