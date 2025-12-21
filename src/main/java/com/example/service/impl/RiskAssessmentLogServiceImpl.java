a9eaabc7f469c0e1816e8f6e65613dbb640f711b
bala-amypo <bala@amypo.in>
2025-12-21 14:47:48 +0000

    User commit

--- src/main/java/com/example/service/impl/RiskAssessmentLogServiceImpl.java
+++ src/main/java/com/example/service/impl/RiskAssessmentLogServiceImpl.java
@@ -14,7 +14,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class RiskAssessmentServiceImpl implements RiskAssessmentService {
+public class RiskAssessmentLogServiceImpl implements RiskAssessmentService {
 
     private final LoanRequestRepository loanRequestRepository;
     private final FinancialProfileRepository profileRepository;
