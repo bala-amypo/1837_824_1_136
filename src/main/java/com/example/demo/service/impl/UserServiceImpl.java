63b269fa39d17d64162027d505a909394ec2d771
bala-amypo <bala@amypo.in>
2025-12-22 08:37:01 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -1,9 +1,9 @@
 package com.example.demo.service.impl;
 
-import org.springframework.stereotype.Service;
-import com.example.demo.service.UserService;
 import com.example.demo.entity.User;
 import com.example.demo.repository.UserRepository;
+import com.example.demo.service.UserService;
+import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -14,11 +14,13 @@ public class UserServiceImpl implements UserService {
         this.repo = repo;
     }
 
-    public User registerUser(User user) {
+    @Override
+    public User save(User user) {
         return repo.save(user);
     }
 
-    public User getById(Long id) {
-        return repo.findById(id).orElseThrow();
+    @Override
+    public User findByEmail(String email) {
+        return repo.findByEmail(email);
     }
 }
