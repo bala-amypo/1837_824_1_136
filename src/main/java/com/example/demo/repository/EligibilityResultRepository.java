8d9569055938fd8d43263eac87d728699a917805
bala-amypo <bala@amypo.in>
2025-12-22 08:10:02 +0000

    User commit

--- src/main/java/com/example/demo/repository/EligibilityResultRepository.java
+++ src/main/java/com/example/demo/repository/EligibilityResultRepository.java
@@ -1,10 +1,12 @@
+package com.java.example.repository;
+
 import com.example.demo.entity.EligibilityResult;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface EligibilityResultRepository
-        extends JpaRepository<EligibilityResult, Long> {
+        extends JpaRepository<EligibilityResult, Long>{
 
     Optional<EligibilityResult> findByLoanRequestId(Long loanRequestId);
 }
