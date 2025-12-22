3a544d4d6ef68e5f3600d81e415bee3fd8eddc5d
bala-amypo <bala@amypo.in>
2025-12-22 08:28:01 +0000

    User commit

--- src/main/java/com/example/demo/controller/LoanRequestController.java
+++ src/main/java/com/example/demo/controller/LoanRequestController.java
@@ -12,26 +12,26 @@
 //     private final LoanRequestService service;
 
 //     public LoanRequestController(LoanRequestService service) {
-//         this.service = service;
-//     }
+         this.service = service;
+     }
 
-//     @PostMapping
-//     public LoanRequest submit(@RequestBody LoanRequest request) {
-//         return service.submitLoanRequest(request);
-//     }
+     @PostMapping
+     public LoanRequest submit(@RequestBody LoanRequest request) {
+         return service.submitLoanRequest(request);
+     }
 
-//     @GetMapping("/user/{id}")
-//     public List<LoanRequest> byUser(@PathVariable Long id) {
-//         return service.getRequestsByUser(id);
-//     }
+     @GetMapping("/user/{id}")
+     public List<LoanRequest> byUser(@PathVariable Long id) {
+         return service.getRequestsByUser(id);
+     }
 
-//     @GetMapping("/{id}")
+     @GetMapping("/{id}")
     public LoanRequest get(@PathVariable Long id) {
-//         return service.getRequestById(id);
-//     }
+         return service.getRequestById(id);
+     }
 
-//     @GetMapping
-//     public List<LoanRequest> all() {
-//         return service.getAllRequests();
-//     }
-// }
+     @GetMapping
+     public List<LoanRequest> all() {
+         return service.getAllRequests();
+     }
+ }
