8a6979d26c4f37ee919b06cc582d7047b107f51d
bala-amypo <bala@amypo.in>
2025-12-22 10:01:00 +0000

    User commit

--- src/main/java/com/example/demo/service/LoanEligibilityService.java
+++ src/main/java/com/example/demo/service/LoanEligibilityService.java
@@ -1,15 +1,5 @@
 package com.example.demo.service;
 
-import org.springframework.stereotype.Service;
-
-@Service
-public class LoanEligibilityService {
-
-    public String checkEligibility(Long income) {
-        if (income >= 25000) {
-            return "Eligible for Loan";
-        } else {
-            return "Not Eligible for Loan";
-        }
-    }
+public interface LoanEligibilityService {
+    String checkEligibility(Long income);
 }
--- src/main/java/com/example/demo/service/impl/LoanEligibilityServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/LoanEligibilityServiceImpl.java
@@ -1,35 +1,16 @@
 package com.example.demo.service.impl;
 
-import com.example.demo.entity.EligibilityResult;
-import com.example.demo.entity.LoanRequest;
-import com.example.demo.repository.EligibilityResultRepository;
-import com.example.demo.repository.LoanRequestRepository;
 import com.example.demo.service.LoanEligibilityService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LoanEligibilityServiceImpl implements LoanEligibilityService {
 
-    private final EligibilityResultRepository repo;
-    private final LoanRequestRepository loanRepo;
-
-    public LoanEligibilityServiceImpl(
-            EligibilityResultRepository repo,
-            LoanRequestRepository loanRepo) {
-        this.repo = repo;
-        this.loanRepo = loanRepo;
+    @Override
+    public String checkEligibility(Long income) {
+        if (income >= 25000) {
+            return "Eligible for Loan";
+        }
+        return "Not Eligible for Loan";
     }
-@Override
-public EligibilityResult getResultByRequest(Long requestId) {
-    LoanRequest loan = loanRepo.findById(requestId).orElseThrow();
-
-    EligibilityResult result = new EligibilityResult();
-    result.setLoanRequest(loan);
-    result.setEligible(true);
-    result.setMaxEligibleAmount(loan.getRequestedAmount() * 0.8);
-
-    return repo.save(result);
-}
-
-  
 }
